2f98b4774d3caaa923f2ea51cc707d3594d9f0b6
Bryan Siegel <bryansiegel@gmail.com>
2020-03-26 12:26:16 -0700

    added laracasts to laravel category

--- wp-content/themes/bryansiegel/category-laravel.php
+++ wp-content/themes/bryansiegel/category-laravel.php
@@ -1,64 +1,66 @@
-<?php 
+<?php
 /*
 Template Name: Laravel Category
 */
 
- ?>
+?>
 
 
- <?php echo get_template_part('partials/head'); ?>
-     <?php echo get_template_part('partials/header'); ?>
+<?php echo get_template_part('partials/head'); ?>
+<?php echo get_template_part('partials/header'); ?>
 <main id="content">
 
-        <!-- Hero -->
-        <div class="container py-5 py-lg-6">
-            <h1 class="display-3 pb-3">Laravel Projects</h1>
-                                    <!-- line -->
-                                    <div class="holder">
-                <div class="line line1"></div>
-                <div class="line line2"></div>
-            </div>
-            <div class="row">
-                <div class="col-lg-8">
+    <!-- Hero -->
+    <div class="container py-5 py-lg-6">
+        <h1 class="display-3 pb-3">Laravel Projects</h1>
+        <p><a href="https://laracasts.com/@bryansiegel" title="Bryan Siegel on Laracasts">@bryansiegel</a> on Laracasts</p>
 
-                </div>
+        <!-- line -->
+        <div class="holder">
+            <div class="line line1"></div>
+            <div class="line line2"></div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8">
+
             </div>
         </div>
+    </div>
 
-                <div class="bg-white text-black pb-5">
-            <div class="container py-2">
-                <div class="row mt-n6">
+    <div class="bg-white text-black pb-5">
+        <div class="container py-2">
+            <div class="row mt-n6">
 
 
-<?php 
+                <?php
 
-$args = array(
-    'cat' => 'laravel',
-    'post_type' => array( 'page', 'post' ),
-    'tax_query' => array(
-            array(
-                'taxonomy' => 'category',
-                'field' => 'slug',
-                'terms' => 'laravel'
-            ),     
-    )
-);
-$laravel_pages = query_posts( $args );
-  foreach ($laravel_pages as $laravel) : ?>
+                $args = array(
+                    'cat' => 'laravel',
+                    'post_type' => array('page', 'post'),
+                    'tax_query' => array(
+                        array(
+                            'taxonomy' => 'category',
+                            'field' => 'slug',
+                            'terms' => 'laravel'
+                        ),
+                    )
+                );
+                $laravel_pages = query_posts($args);
+                foreach ($laravel_pages as $laravel) : ?>
 
-<div class="col-md-6">
+                    <div class="col-md-6">
                         <div class="card border-0 mb-5">
                             <a href="<?php echo get_page_link($laravel->ID); ?>" class="h-100 img-rising" aria-hidden="true">
 
                                 <img width="357" height="353" src="
-                                <?php 
-                                $url = get_the_post_thumbnail_url( $laravel->ID, 'large' );
-                                echo $url; ?>" class="card-img-top wp-post-image">                            </a>
+                                <?php
+                                $url = get_the_post_thumbnail_url($laravel->ID, 'large');
+                                echo $url; ?>" class="card-img-top wp-post-image"> </a>
                             <div class="card-body px-0">
                                 <h2 class="card-title h3">
                                     <a href="<?php echo get_page_link($laravel->ID); ?>"><?php echo $laravel->post_title; ?></a>
                                 </h2>
-                                
+
 
                             </div>
                         </div>
@@ -68,25 +70,25 @@ $laravel_pages = query_posts( $args );
 
 
 
+            </div>
         </div>
-    </div>
 
 
         <!-- Articles -->
-        
-                <nav aria-label="Page navigation">
-                    <div class="pagination justify-content-center">
 
-<?php the_posts_pagination( array(
-    'mid_size' => 4,
-    'prev_text' => __( 'Previous', 'textdomain' ),
-    'next_text' => __( 'Next', 'textdomain' ),
-) ); ?>
+        <nav aria-label="Page navigation">
+            <div class="pagination justify-content-center">
 
-                    </div>
-                </nav>
+                <?php the_posts_pagination(array(
+                    'mid_size' => 4,
+                    'prev_text' => __('Previous', 'textdomain'),
+                    'next_text' => __('Next', 'textdomain'),
+                )); ?>
+
             </div>
-        </div>
-    </main>
-             <?php echo get_template_part('partials/call-to-action'); ?>
-     <?php echo get_template_part('partials/footer'); ?>
+        </nav>
+    </div>
+    </div>
+</main>
+<?php echo get_template_part('partials/call-to-action'); ?>
+<?php echo get_template_part('partials/footer'); ?>
